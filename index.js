2abfd395f5a281493e99ceccc48910c36b24338c
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2023-09-19 20:26:03 +0700

    try method request with native way

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
- 
     response.statusCode = 200;
-    response.end('<h1>Halo HTTP Server!</h1>');
+ 
+    const { method } = request;
+
+    if(method === "GET"){
+        response.end('<h1>Halo HTTP Server!</h1>');
+    }
+    if(method === "PUT"){
+        response.end('<h1>Bonjour!</h1>');
+    }
+    if(method === "POST"){
+        response.end('<h1>OLA!</h1>');
+    }
+    if(method === "DELETE"){
+        response.end('<h1>KONICHIWA!</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);
